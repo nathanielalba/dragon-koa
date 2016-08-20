a096d4dde43b79198c2725b937e87a3d4a72eb51
Nathaniel Alba <nathaniel.alba@gmail.com>
2016-08-20 02:27:00 -0700

    add bootstrap sass for custom styling

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App.jsx';
 
+require('!style!css!sass!resolve-url!sass?sourceMap!applicationStyles');
+
 ReactDOM.render(
   <App />,
   document.getElementById('app')
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,41 @@
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
-  entry: ['./src/app.jsx'],
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!bootstrap-sass/assets/javascripts/bootstrap.min.js',
+    './src/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
     path: './public',
     filename: 'bundle.js'
   },
+  resolve: {
+    root: __dirname,
+    modulesDirectories: [
+      'node_modules',
+      './src/components'
+    ],
+    alias: {
+      applicationStyles: 'src/styles/app.scss'
+    }
+  },
   module: {
     loaders: [
+      {
+        loader: 'url-loader?limit=100000',
+        test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/
+      },
       {
         loader: 'babel-loader',
         query: {
@@ -15,5 +45,14 @@ module.exports = {
         exlude: /node_modules/
       }
     ]
+  },
+  sassLoader: {
+    includePaths: [
+      path.resolve(__dirname, './node_modules/bootstrap-sass/assets/stylesheets'),
+      path.resolve(__dirname, './node_modules/bootstrap-sass/assets/fonts')
+    ]
+  },
+  postcss: function() {
+    return [require('autoprefixer'), require('precss')]
   }
-}
+};
